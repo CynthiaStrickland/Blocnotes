9a7fec20a2f04e887bbbc9156192375eea261ce1
Cynthia Whitlatch <cawhitlatch2@gmail.com>
2015-06-25 10:34:08 -0700

    blocnotes0625

--- Note.swift
+++ Note.swift
@@ -8,6 +8,34 @@
 
 import UIKit
 
+//GLOBAL variables to manage the data - class variables, as of this writing, not supported in SWIFT
+
+var allNotes:[Note] = []
+
+//creating an array that represents all the notes created
+
+var currentNoteIndex:Int = -1
+
+//This refers to the current index in the note array.  So once you select a note to edit the current note index is going to save the note you are editing.
+
+var noteTable:UITableView?
+
+//References the table of notes.  No way to instantiate the note table inside of this class because there's no access until the app launches and the view loads.  Use a ! or a ? if you can't instantiate it and need to declare it as optional
+
+
+
 class Note: NSObject {
-   
+    var date:String
+    var note:String
+    
+    //For variables that you can't initialize right away you need to declare that they can be optional using ! or ?.
+    
+    // () = constructor    Gives a date based on the current time.  Need to be able to convert it to a string rather than an NSDate.   Use DESCRIPTION property to do this.
+    
+    //  When you use the init methon in your sub-classing and other class you need to override the init method.
+    
+    override init() {
+        date = NSDate().description
+        note = ""
+    }
 }
--- blocnotes/MasterViewController.swift
+++ blocnotes/MasterViewController.swift
@@ -12,7 +12,21 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        noteTable = self.UITableView
+        
+        //Make a new note.   In Swift you don't have to call any import methods to import classes that are contained within the project.
+        
+//        var n:Note = Note()
+//        println("note date is \(n.date)")   // date is the variable from Note.swift
+//          Commented out because it was used as an example.   
+        
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.navigationItem.leftBarButtonItem = self.editButtonItem()
+        
+        let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
+        self.navigationItem.rightBarButtonItem = addButton
+
     }
 
     override func didReceiveMemoryWarning() {
