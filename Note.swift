66cc8a885e47c481fe5b217a54bec03c128b62f0
Cynthia Whitlatch <cawhitlatch2@gmail.com>
2015-06-25 10:33:27 -0700

    blocnotes0625

---
+++ Note.swift
@@ -0,0 +1,13 @@
+//
+//  Note.swift
+//  blocnotes
+//
+//  Created by Cynthia Whitlatch on 6/25/15.
+//  Copyright (c) 2015 Cynthia Whitlatch. All rights reserved.
+//
+
+import UIKit
+
+class Note: NSObject {
+   
+}
